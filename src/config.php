3a2e0da32fa82e73316a281fd3d832b29decd17c
Kevin <stradtkt22@gmail.com>
2019-02-19 00:57:41 -0500

    Added an autoloader

--- src/config.php
+++ src/config.php
@@ -5,3 +5,13 @@
  * Date: 2/19/2019
  */
 
+function autoloader($class_name) {
+    foreach (glob(__DIR__ . '/*', GLOB_ONLYDIR) as $dir) {
+        if(file_exists("$dir/" . $class_name . '.php')) {
+            require_once "$dir/" . $class_name . '.php';
+            break;
+        }
+    }
+}
+
+spl_autoload_register('autoloader');
