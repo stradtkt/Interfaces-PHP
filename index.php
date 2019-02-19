57a67ca19abae9bdcbe0a6674f77a4d5791e546d
Kevin <stradtkt22@gmail.com>
2019-02-19 00:51:34 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require_once "src/config.php";
+$content = new Collection();
+$title = "My Website";
+
+require 'views/header.php';
+
+require 'views/footer.php';
---
+++ src/config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 2/19/2019
+ */
+
