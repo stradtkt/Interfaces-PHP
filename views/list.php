be1914448fe744ede9e286ef603a7c38645c2c0e
Kevin <stradtkt22@gmail.com>
2019-02-19 06:01:31 -0500

    Added share buttons

--- views/list.php
+++ views/list.php
@@ -3,6 +3,9 @@
 if($item->status == "published") {
     echo '<article>';
     echo '<h2><a href="?id=' . $item['id'] . '">' . $item->title . '</a></h2>';
+    if($content instanceof TrackableInterface) {
+        include __DIR__ . "/track.php";
+    }
     echo '<p>' . $content->shortDescription() . '</p>';
     echo '</article>';
 }
--- views/single.php
+++ views/single.php
@@ -5,7 +5,14 @@
  * Date: 2/19/2019
  * Time: 3:04 AM
  */
-
+echo '<div>';
+if($content instanceof TrackableInterface) {
+    include __DIR__ . "/track.php";
+}
+if($content instanceof ShareableInterface) {
+    include __DIR__ . "/share.php";
+}
+echo '</div>';
 echo '<article>';
 echo $content->current()->details;
 echo '</article>';
